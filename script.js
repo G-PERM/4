9ad049d9df4737a56f9ecca1c8f33982aac12ef2
G-PERM <gpermweb@gmail.com>
2025-12-04 11:00:57 +0530

    The Formatting

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-function calculate() {
-  let l = parseFloat(document.getElementById("l").value);
-  let b = parseFloat(document.getElementById("b").value);
-  if (l > 0 && b > 0) {
-    let a = l * b;
-    document.getElementById("answer").innerHTML =
-      "Area of a rectangle with length " +
-      l +
-      " units and breadth " +
-      b +
-      " units is " +
-      a +
-      " unit².";
-  } else {
-    document.getElementById("answer").innerHTML =
-      "Please enter a number which is greater than zero.";
-  }
-}
+function calculate(){let e=parseFloat(document.getElementById("l").value),t=parseFloat(document.getElementById("b").value);if(e>0&&t>0){let n=e*t;document.getElementById("answer").innerHTML="Area of a rectangle with length "+e+" units and breadth "+t+" units is "+n+" unit²."}else document.getElementById("answer").innerHTML="Please enter a number which is greater than zero."}
