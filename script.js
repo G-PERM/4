fcb721bbf97d4f5c693659fd2cccf8da8a3b8243
G-PERM <gpermweb@gmail.com>
2025-12-04 10:29:54 +0530

    Formatting

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 function calculate() {
-    let l = parseFloat(document.getElementById("l").value);
-    let b = parseFloat(document.getElementById("b").value);
-    if ((l>0) && (b>0)) { 
-        let a = l * b;
-        document.getElementById("answer").innerHTML = "Area of a rectangle with length " + l + " units and breadth " + b + " units is " + a + " unit².";
-    }
-    else {
-        document.getElementById("answer").innerHTML = "Please enter a number which is greater than zero.";
-    }
+  let l = parseFloat(document.getElementById("l").value);
+  let b = parseFloat(document.getElementById("b").value);
+  if (l > 0 && b > 0) {
+    let a = l * b;
+    document.getElementById("answer").innerHTML =
+      "Area of a rectangle with length " +
+      l +
+      " units and breadth " +
+      b +
+      " units is " +
+      a +
+      " unit².";
+  } else {
+    document.getElementById("answer").innerHTML =
+      "Please enter a number which is greater than zero.";
+  }
 }
